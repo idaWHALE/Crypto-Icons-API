b790698d2d5132bd0f81d480029b34b1c01f0c1b
idaWHALE <eagidawhale@gmail.com>
2019-11-07 14:57:38 -0800

    Update with submodule of icons project
    Remove old icons and point to look in submodule
    Add size max limit of 1000

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const { convert } = require('convert-svg-to-png')
+const express = require('express');
+const app = express();
+const { convert } = require('convert-svg-to-png');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const { document } = (new JSDOM('')).window
+const { document } = (new JSDOM('')).window;
 const fs = require('fs');
 const path = require('path');
 
@@ -13,19 +13,19 @@ app.use(express.static(__dirname + '/public'));
 // GET Home page
 app.get('/', function(req, res) {
   res.sendFile('index.html');
-})
+});
 
 // GET png
 app.get('/api/:style/:currency/:size/:color?', async(req, res) => {
   // Params
-  const style = req.params.style
-  const currency = req.params.currency
-  const size = req.params.size
-  const cacheKey = req.path
-  const filename = currency + '-' + style + '-' + size + '.png'
+  const style = req.params.style;
+  const currency = req.params.currency;
+  const size = req.params.size;
+  const cacheKey = req.path;
+  const filename = currency + '-' + style + '-' + size + '.png';
 
-  // Validate size is > 0
-  if (size <= 0) {
+  // 0 <= Valid size <= 1000
+  if (size <= 0 || size >= 1000) {
     res.status(400).send({'error' : 'Invalid size'});
     return
   }
@@ -33,55 +33,55 @@ app.get('/api/:style/:currency/:size/:color?', async(req, res) => {
   // Redis
   var redisRetryStrategy = function(options) {
     if (options.error.code === 'ECONNREFUSED') {
-      return
-    }
-  }
 
-  const redisURL = process.env.REDIS_URL || 'http://127.0.0.1:6379'
+    }
+  };
+
+  const redisURL = process.env.REDIS_URL || 'http://127.0.0.1:6379';
   var client = require('redis').createClient({
     url : process.env.REDIS_URL,
     return_buffers : true
-  })
+  });
 
   client.on('error', function (err) {
-    client.quit()
+    client.quit();
     generatePNG(req, res, null)
-  })
+  });
 
   client.on('connect', function (err) {
     // Check cache
     client.get(cacheKey, async(error, result) => {
       if (result == null) {
-        console.log("Cache miss")
+        console.log("Cache miss");
         generatePNG(req, res, client)
       } else {
-        client.quit()
-        console.log("Cache hit")
+        client.quit();
+        console.log("Cache hit");
         sendPNG(res, result, filename)
       }
     })
   })
-})
+});
 
 // Functions
 
 function sendPNG(response, png, filename) {
-  response.set('Content-Type', 'image/png')
-  response.header('Content-disposition', 'inline; filename=' + filename)
+  response.set('Content-Type', 'image/png');
+  response.header('Content-disposition', 'inline; filename=' + filename);
   response.send(png)
 }
 
 async function generatePNG(req, res, redis) {
   // Params
-  const style = req.params.style
-  const currency = req.params.currency
-  const size = req.params.size
-  const color = req.params.color
-  const cacheKey = req.path
-  const filename = currency + '-' + style + '-' + size + '.png'
+  const style = req.params.style;
+  const currency = req.params.currency;
+  const size = req.params.size;
+  const color = req.params.color;
+  const cacheKey = req.path;
+  const filename = currency + '-' + style + '-' + size + '.png';
 
   // SVG file path
-  const svgPath = path.join(__dirname, 'public', 'svg', style, currency + '.svg');
+  const svgPath = path.join(__dirname, 'public', 'cryptocurrency-icons', 'svg', style, currency + '.svg');
 
   // Check if file exists
   if (!fs.existsSync(svgPath)) {
@@ -90,29 +90,29 @@ async function generatePNG(req, res, redis) {
   }
 
   const svg = fs.readFileSync(svgPath, 'utf8');
-  const element = document.createElement('div')
-  element.innerHTML = svg
+  const element = document.createElement('div');
+  element.innerHTML = svg;
 
-  const svgElement = element.getElementsByTagName("svg")[0]
+  const svgElement = element.getElementsByTagName("svg")[0];
 
   // Define the circles
-  const colorCircle = element.getElementsByTagName("circle")[0]
-  const iconCircle = element.getElementsByTagName("use")[1]
+  const colorCircle = element.getElementsByTagName("circle")[0];
+  const iconCircle = element.getElementsByTagName("use")[1];
 
   // Set viewBox so SVG resizes correctly
-  const originalSize = svgElement.getAttribute('width')
-  svgElement.setAttribute('viewBox', '0 0 ' + originalSize + ' ' + originalSize)
+  const originalSize = svgElement.getAttribute('width');
+  svgElement.setAttribute('viewBox', '0 0 ' + originalSize + ' ' + originalSize);
 
   // Set requested size
-  svgElement.setAttribute('width', size)
-  svgElement.setAttribute('height', size)
+  svgElement.setAttribute('width', size);
+  svgElement.setAttribute('height', size);
 
   // Set circle color, if `color` or `icon`
   if (color != null && style == 'color') {
-    const colorString = '#' + color
+    const colorString = '#' + color;
     colorCircle.setAttribute('fill', colorString)
   } else if (color != null && style == 'icon') {
-    const colorString = '#' + color
+    const colorString = '#' + color;
     iconCircle.setAttribute('fill', colorString)
   }
 
@@ -136,4 +136,4 @@ async function generatePNG(req, res, redis) {
 
 // Listen
 var port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Our app is running on http://localhost:' + port))
+app.listen(port, () => console.log('Our app is running on http://localhost:' + port));
